890428d382baa29ab0feb1effbe68c08016f3077
Ugiomoh Daniel <ugiomohd@yahoo.com>
2024-03-25 10:50:03 +0100

    details

--- src/components/AuthDetails.jsx
+++ src/components/AuthDetails.jsx
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 const AuthDetails = () => {
-  const [user, setUser] = useState(null);
+  const [authUser, setAuthUser] = useState(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+
+    return () => {
+      listen();
+    };
+  }, []);
+
+  const userSignOut = () => {
+    signOut()
+      .then(() => {
+        console.log("Signed Out Successfully");
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
       <div>
-        <h1>AuthDetails</h1>
+        <h1>
+          {authUser ? (
+            <>
+              <p>{`Signed In as ${authUser.email}`}</p>{" "}
+              <button onClick={userSignOut}>Sign Out</button>
+            </>
+          ) : (
+            <p>Not Signed In</p>
+          )}
+        </h1>
       </div>
     </>
   );
